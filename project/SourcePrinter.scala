32fc725a756a7f65c45a9a992e6e02d7dd4eb372
VladimirNik <vladimir.nikolaev9@gmail.com>
2013-09-26 05:41:43 +0400

    trying set up jekyll

--- project/SourcePrinter.scala
+++ project/SourcePrinter.scala
@@ -3,6 +3,11 @@ import Keys._
 import Configurations.CompilerPlugin
 import sbtassembly.Plugin._
 import AssemblyKeys._
+import com.typesafe.sbt.SbtSite.site
+import com.typesafe.sbt.SbtSite.SiteKeys._
+import com.typesafe.sbt.site.JekyllSupport.Jekyll
+import com.typesafe.sbt.SbtGhPages.ghpages
+import com.typesafe.sbt.SbtGit.git
 
 object SourcePrinter extends Build {
   val printPluginName = "printplugin"
@@ -20,14 +25,27 @@ object SourcePrinter extends Build {
     ) 
 
     val printPlugin = Project(printPluginName, file("."),
-      settings = buildSettings ++ assemblySettings ++ assemblyProjectSettings ++ 
+      settings = buildSettings ++ assemblySettings ++ assemblyProjectSettings ++ websiteSettings ++
       addArtifact(Artifact(printPluginName, "assembly"), sbtassembly.Plugin.AssemblyKeys.assembly)) settings (
 	     name := printPluginName,
 
       //crossVersion := CrossVersion.full,
       //exportJars := true,
       libraryDependencies <++= scalaVersion apply dependencies
-    )
+    ) 
+
+    lazy val websiteSettings: Seq[Setting[_]] = (
+     site.settings ++
+     ghpages.settings ++
+     site.includeScaladoc() ++
+     site.jekyllSupport() ++
+     Seq(
+       git.remoteRepo := "https://github.com/VladimirNik/printPlugin.git",
+       includeFilter in Jekyll := ("*.html" | "*.png" | "*.js" | "*.css" | "CNAME")
+       // the migration guide goes to scaladoc
+       //excludeFilter in Jekyll := ("actors-migration-guide.html")
+     )
+   )
 
     def dependencies(sv: String) = Seq(
       "org.scala-lang" % "scala-compiler" % sv,
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,3 +1,9 @@
 addSbtPlugin("com.github.mpeltonen" % "sbt-idea" % "1.5.1")
 
 addSbtPlugin("com.eed3si9n" % "sbt-assembly" % "0.9.2")
+
+resolvers += "jgit-repo" at "http://download.eclipse.org/jgit/maven"
+
+addSbtPlugin("com.typesafe.sbt" % "sbt-ghpages" % "0.5.0")
+
+addSbtPlugin("com.typesafe.sbt" % "sbt-site" % "0.6.2")
